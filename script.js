f28c71acbcdf5ac90fc18d97c5f7dba1e9400b8e
varunkewlani <91694985+varunkewlani@users.noreply.github.com>
2024-07-16 19:55:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ let value1 = prompt("Enter Start of the Range.");
 let value2 = prompt("Enter End Of the Range.");
 function isSameType(value1, value2)
 {
-	if(typeof(value1)==typeof(value2) )
+	if(typeof value1 === typeof value2)
 		return true;
-	elseif(isNaN(value1) && isNaN(value2))
+	else if(isNaN(value1) && isNaN(value2))
 		return true;
 	else{
 		return false;

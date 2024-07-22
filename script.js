4f75db88ee0aa8b29e362fc56206ec11ccb83d2b
varunkewlani <91694985+varunkewlani@users.noreply.github.com>
2024-07-22 16:59:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-function isSameType(value1, value2)
-{
-	if(Number.isNaN(value1) && Number.isNaN(value2))
-		return true;
-	else if(typeof(value1) === typeof(value2))
-		return true;
+function isSameType(value1, value2) {
+    // Convert to numbers if possible
+    if (!isNaN(value1)) {
+        value1 = Number(value1);
+    }
+    if (!isNaN(value2)) {
+        value2 = Number(value2);
+    }
 
-	else{
-		return false;
-	}
-}
-let value1 = prompt()
-let value2 = prompt()
-if (!isNaN(value1)) {
-    value1 = Number(value1);
-}
-if (!isNaN(value2)) {
-    value2 = Number(value2);
+    // Check for NaN
+    if (Number.isNaN(value1) && Number.isNaN(value2)) {
+        return true;
+    }
+    // Compare types
+    return typeof value1 === typeof value2;
 }
 
+// Prompt inputs and alert the result
+let value1 = prompt("Enter the first value:");
+let value2 = prompt("Enter the second value:");
 alert(isSameType(value1, value2));

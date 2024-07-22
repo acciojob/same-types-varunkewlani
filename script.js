bf020c8c7f5b43d7ccbda366d80ff43c522747f7
varunkewlani <91694985+varunkewlani@users.noreply.github.com>
2024-07-22 16:54:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,14 +2,13 @@ function isSameType(value1, value2)
 {
 	if(Number.isNaN(value1) && Number.isNaN(value2))
 		return true;
-	else if(typeof(value1) == typeof(value2))
+	else if(typeof(value1) === typeof(value2))
 		return true;
-		
+
 	else{
 		return false;
 	}
 }
-let value1 = prompt();
-let value2 = prompt();
-
+let value1 = prompt()
+let value2 = prompt()
 alert(isSameType(value1, value2))
